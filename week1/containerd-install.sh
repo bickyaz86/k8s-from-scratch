a02ecd39845bc80644731e445c8f0622d4751a82
bickyaz86 <114374997+bickyaz86@users.noreply.github.com>
2022-10-09 19:13:39 +0530

    Update containerd-install.sh

--- week1/containerd-install.sh
+++ week1/containerd-install.sh
@@ -1,8 +1,9 @@
-# install the containerd runtime
-sudo apt-get install containerd -y
-
+# install the containerd on ubuntu server
+  # 1. Download the Containerd runtime with the command:
+       wget https://github.com/containerd/containerd/releases/download/v1.6.8/containerd-1.6.8-linux-amd64.tar.gz
+  # 2. Unpack that file into /usr/local/ with the command: 
+       sudo tar Cxzvf /usr/local containerd-1.6.8-linux-amd64.tar.gz    
 # Configure containerd and start the service
 sudo mkdir -p /etc/containerd
 sudo su -
 containerd config default  /etc/containerd/config.toml
-
