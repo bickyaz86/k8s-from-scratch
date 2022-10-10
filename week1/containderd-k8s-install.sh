6c623d8af5d8caf18e7ede26a18818ff9abbff56
bickyaz86 <114374997+bickyaz86@users.noreply.github.com>
2022-10-10 15:14:07 +0530

    Update containderd-k8s-install.sh

--- week1/containderd-k8s-install.sh
+++ week1/containderd-k8s-install.sh
@@ -1,11 +1,11 @@
 # Install kubeadm,kubelet & Kubectl
    # 1. Update the apt package index and install packages needed to use the Kubernetes apt repository:
-	          sudo apt-get update
-            sudo apt-get install -y apt-transport-https ca-certificates curl
-	 # 2. Download the Google Cloud public signing key:
-	          sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-	 # 3. Add the Kubernetes apt repository:
-	          echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+        sudo apt-get update
+        sudo apt-get install -y apt-transport-https ca-certificates curl
+   # 2. Download the Google Cloud public signing key:
+	sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+   # 3. Add the Kubernetes apt repository:
+	echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
    # 4.Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
             sudo apt-get update
             sudo apt-get install -y kubelet kubeadm kubectl
