67e25515b2bf50df4ee5b05e5adddf5383af1811
bickyaz86 <114374997+bickyaz86@users.noreply.github.com>
2022-10-10 11:59:21 +0530

    Update containerd-deploy-vm.sh

--- week1/containerd-deploy-vm.sh
+++ week1/containerd-deploy-vm.sh
@@ -4,7 +4,7 @@ location="CentralIndia"
 deploymentName="vm-deploy"
 
 # download template
-wget https://raw.githubusercontent.com/chadmcrowell/k8s-from-scratch/main/week1/containerd-single-node.json -O template.json
+wget https://raw.githubusercontent.com/bickyaz86/k8s-from-scratch/main/week1/containerd-single-node.json -O template.json
 
 # create resource group
 az group create -n $resourceGroupName -l $location
